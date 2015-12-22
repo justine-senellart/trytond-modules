2fcbbd601e23b92d3121d53968a65eb9b01b9168
jeremy <jeremy@jeremy-ThinkPad-S5-S540>
2015-12-22 10:40:10 +0100

    commission : fix states commissions page

--- modules/commission/invoice.py
+++ modules/commission/invoice.py
@@ -210,7 +210,7 @@ class InvoiceLine:
     @classmethod
     def view_attributes(cls):
         return super(InvoiceLine, cls).view_attributes() + [
-            ('//page[@id="commissions"]', 'state', {
+            ('//page[@id="commissions"]', 'states', {
                     'invisible': Eval('type') != 'line',
                     })]
 
