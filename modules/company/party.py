965fbb9cb7b0a6e9a360458e4822cee7c000d5c2
Frederic Langevin <frederic.langevin@coopengo.com>
2016-01-27 16:43:55 +0100

    party: Fix #3337 Initialized party lang based on party configuration http://rietveld.coopengo.com/15940001 https://bugs.tryton.org/issue5198

--- modules/company/party.py
+++ modules/company/party.py
@@ -1,7 +1,8 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.pyson import Eval
-from trytond.pool import PoolMeta
+from trytond.pool import PoolMeta, Pool
+from trytond.transaction import Transaction
 
 __all__ = ['PartyConfiguration']
 __metaclass__ = PoolMeta
@@ -18,3 +19,36 @@ class PartyConfiguration:
             cls.party_sequence.domain,
             ('company', 'in', [Eval('context', {}).get('company'), None]),
             ]
+
+    def get_party_lang(self, name):
+        pool = Pool()
+        Property = pool.get('ir.property')
+        company_id = Transaction().context.get('company')
+        lang_field = self._get_lang_field(name)
+        properties = Property.search([
+                ('field', '=', lang_field.id),
+                ('res', '=', None),
+                ('company', '=', company_id),
+                ], limit=1)
+        if properties:
+            prop, = properties
+            return prop.value.id
+
+    @classmethod
+    def set_party_lang(cls, configurations, name, value):
+        pool = Pool()
+        Property = pool.get('ir.property')
+        company_id = Transaction().context.get('company')
+        lang_field = cls._get_lang_field(name)
+        properties = Property.search([
+                ('field', '=', lang_field.id),
+                ('res', '=', None),
+                ('company', '=', company_id),
+                ])
+        Property.delete(properties)
+        if value:
+            Property.create([{
+                        'field': lang_field.id,
+                        'value': 'ir.lang,%s' % value,
+                        'company': company_id,
+                        }])
--- modules/party/configuration.py
+++ modules/party/configuration.py
@@ -1,6 +1,7 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, ModelSingleton, fields
+from trytond.pool import Pool
 
 __all__ = ['Configuration']
 
@@ -13,6 +14,46 @@ class Configuration(ModelSingleton, ModelSQL, ModelView):
             'Party Sequence', domain=[
                 ('code', '=', 'party.party'),
                 ]))
-    party_lang = fields.Property(fields.Many2One("ir.lang", 'Party Language',
+    # change tryton behavior in order to automatically initialize party lang
+    party_lang = fields.Function(fields.Many2One('ir.lang', 'Party Language',
         help=('The value set on this field will preset the language on new '
-            'parties')))
+            'parties')),
+        'get_party_lang', setter='set_party_lang')
+
+    @classmethod
+    def _get_lang_field(cls, name):
+        pool = Pool()
+        ModelField = pool.get('ir.model.field')
+        field, = ModelField.search([
+            ('model.model', '=', 'party.party'),
+            ('name', '=', 'lang'),
+            ], limit=1)
+        return field
+
+    def get_party_lang(self, name):
+        pool = Pool()
+        Property = pool.get('ir.property')
+        lang_field = self._get_lang_field(name)
+        properties = Property.search([
+                ('field', '=', lang_field.id),
+                ('res', '=', None),
+                ], limit=1)
+        if properties:
+            prop, = properties
+            return prop.value.id
+
+    @classmethod
+    def set_party_lang(cls, configurations, name, value):
+        pool = Pool()
+        Property = pool.get('ir.property')
+        lang_field = cls._get_lang_field(name)
+        properties = Property.search([
+                ('field', '=', lang_field.id),
+                ('res', '=', None),
+                ])
+        Property.delete(properties)
+        if value:
+            Property.create([{
+                        'field': lang_field.id,
+                        'value': 'ir.lang,%s' % value,
+                        }])
